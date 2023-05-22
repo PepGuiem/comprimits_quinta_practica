d6f3212a149fe3892aae379c6f51253d8c503570
PepGuiem <82808082+PepGuiem@users.noreply.github.com>
2023-05-22 23:59:25 +0200

    lzw compress done (falta natetjar)

--- src/RLE.java
+++ src/RLE.java
@@ -1,58 +1,45 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 public class RLE {
-    public static void compress(InputStream is, OutputStream os) {
-        try {
-            byte[] bytes = is.readAllBytes();
-            byte cont = 0;
-            int contNum = 0;
-            boolean si;
-
-            for (int i = 0; i < bytes.length; i++) {
-                if (i >= 1 && bytes[i - 1] == bytes[i]) {
-                    contNum++;
-                    si = true;
-                    if (contNum == 257) {
-                        os.write(cont);
-                        cont = 0;
-                        contNum = 0;
-                    }
-                } else {
-                    si = false;
-                }
-                if (contNum > 1 && si) {
-                    cont++;
-                } else {
-                    if (cont > 0) {
-                        os.write(cont);
-                        cont = 0;
-                        contNum = 0;
-                        if (i + 1 == bytes.length && bytes[i - 1] != bytes[i]) os.write(bytes[i]);
-                    } else {
-                        os.write(bytes[i]);
-                        if (seHaDeAfegirElContador(bytes, i)) {
-                            os.write(cont);
-                            cont = 0;
-                            contNum = 0;
-                        }
-                    }
-
+    public static void compress(InputStream is, OutputStream os) throws IOException {
+        byte[] bytes = is.readAllBytes();
+        int cont = 0;
+        int contNum = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            if (i >= 1 && bytes[i - 1] == bytes[i]) {
+                contNum++;
+                if (contNum == 257) {
+                    os.write(cont);
+                    cont = 0;
+                    contNum = 0;
                 }
             }
-            if (cont > 0) {
-                os.write(cont);
+            if (contNum > 1 && bytes[i - 1] == bytes[i]) {
+                cont++;
+            } else {
+                if (cont != 0) {
+                    os.write(cont);
+                    os.write(bytes[i]);
+                    cont = 0;
+                    contNum = 0;
+                } else {
+                    os.write(bytes[i]);
+                    if (seHaDeAfegirElComptador(bytes, i)) {
+                        os.write(cont);
+                        contNum = 0;
+                    }
+                }
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-
+        if (cont > 0) {
+            os.write(cont);
+        }
+        os.close();
     }
 
-    private static boolean seHaDeAfegirElContador(byte[] bytes, int i) {
+    private static boolean seHaDeAfegirElComptador(byte[] bytes, int i) {
         return (i >= 1 && bytes[i - 1] == bytes[i]) && ((i + 1 < bytes.length && bytes[i] != bytes[i + 1])
                 || i + 1 == bytes.length);
     }
@@ -61,24 +48,27 @@ public class RLE {
         try {
             byte[] bytes = is.readAllBytes();
             for (int i = 0; i < bytes.length; i++) {
-                if (i > 0 && bytes[i - 1] == bytes[i]){
+                if (i > 0 && bytes[i - 1] == bytes[i] && i + 1 < bytes.length) {
                     os.write(bytes[i]);
-                    afegirNomres(bytes[i], bytes[i+1], os);
+                    System.out.println(bytes[i]);
+                    afegirNombres(bytes[i], bytes[i + 1], os);
                     i++;
-                }else{
+                } else {
                     os.write(bytes[i]);
+                    System.out.println(bytes[i]);
                 }
             }
+            os.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void afegirNomres(byte aByte, byte aByte1, OutputStream os) throws IOException {
-        int temp = aByte1;
-        if (temp < 0) temp += 256;
-        for (int i = 0; i < temp ; i++) {
+    private static void afegirNombres(byte aByte, byte aByte1, OutputStream os) throws IOException {
+        int temp = aByte1 & 0xff;
+        for (int i = 0; i < temp; i++) {
             os.write(aByte);
+            System.out.println(aByte);
         }
     }
 }
